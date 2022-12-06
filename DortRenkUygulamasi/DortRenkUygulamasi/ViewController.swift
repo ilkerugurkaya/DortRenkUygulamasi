080d58e1654876cc0fd6a54f02fccd697c02b905
ilkerugurkaya <112544173+ilkerugurkaya@users.noreply.github.com>
2022-12-06 09:32:07 +0300

    Add files via upload

---
+++ DortRenkUygulamasi/DortRenkUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DortRenkUygulamasi
+//
+//  Created by İlker Kaya on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
